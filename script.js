93d4c6a1062148892009606f2ad998e01274091c
Samuel Cavalcanti <79030312+samuelcavalcantii@users.noreply.github.com>
2022-01-02 19:42:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function calcular (){
+  var num1 = document.getElementById('num1').value;
+
+  var num2 = document.getElementById('num2').value;
+
+  if (num1 == ''|| num1 == null){
+      alert ('TEM QUE ESCREVE RUM NÚMERO NÉ????')
+      document.getElementById('num1').focus()
+      return false
+    }
+  if (num2 == ''|| num2 == null){
+    alert ('TEM QUE ESCREVE RUM NÚMERO NÉ????')
+    document.getElementById('num2').focus()
+    return false
+    }
+  var resultado = null
+
+  num1 = Number(num1)
+  num2 = Number(num2)
+  
+    
+
+  resultado1 = Math.tan(num1*(Math.PI/180))
+  resultado2 = Math.cos(num2*(Math.PI/180))
+  resultado = Math.round(Math.atan((resultado1 * resultado2))/(Math.PI/180),2)
+  /*resultado = Math.atan(resultado3)*(Math.PI/180)*/
+
+  document.getElementById('resultado').value = resultado
+}
+// // function limpar () {
+//   document.getElementById('resultado').value= "resultado";
+// }
+
