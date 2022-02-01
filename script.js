c5fb6c0bd2315ad3e5d8d3e33cfeb4a9c270bb22
Samuel Cavalcanti <79030312+samuelcavalcantii@users.noreply.github.com>
2022-02-01 18:16:29 +0000

    Adicionei novo input para cálculo do exagero vertical.

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function calcular (){
   var num1 = document.getElementById('num1').value;
 
   var num2 = document.getElementById('num2').value;
+  
+  var num3 = document.getElementById('num3').value;
 
   if (num1 == ''|| num1 == null){
       alert ('Digite um valor.')
@@ -25,19 +27,27 @@ function calcular (){
     document.getElementById('num2').focus()
     return false
     }
+  if (num3 == ''|| num2 == null){
+    alert ('Digite um valor.')
+    document.getElementById('num3').focus()
+    return false
+    }
   var resultado = null
 
   num1 = Number(num1)
   num2 = Number(num2)
+  num3 = Number(num3)
   
     
 
   resultado1 = Math.tan(num1*(Math.PI/180))
   resultado2 = Math.cos(num2*(Math.PI/180))
-  resultado = Math.round(Math.atan((resultado1 * resultado2))/(Math.PI/180),2)
+  resultado = Math.round(Math.atan((resultado1 * resultado2))/(Math.PI/180))
   /*resultado = Math.atan(resultado3)*(Math.PI/180)*/
+  exagero_vertical =Math.atan( Math.tan(resultado * (Math.PI/180)) * num3)/(Math.PI/180)
 
-  document.getElementById('resultado').value = (`${resultado}°`)
+  document.getElementById('resultado1').value = (`${resultado}°`)
+  document.getElementById('resultado2').value = (`${Math.round(exagero_vertical, 1)}°`)
 }
 //function limpar () {
   //document.getElementById('resultado').value= "resultado";
